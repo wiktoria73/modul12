cb6a440563b27f11a5730b553ff8e35ba4b119e2
W <wpawlik73@gmail.com>
2017-06-26 23:32:08 +0200

    test1

--- scripts.js
+++ scripts.js
@@ -1,42 +1,24 @@
-//
-$(document).ready(function() {
-    getQuote();
-    $('.trigger').click(function() {
-        getQuote();
-    })
-});
-//
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries')
 
-var prefix = "https://cors-anywhere.herokuapp.com/";
+$('#search').click(searchCountries);
 
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+function searchCountries() {
+  var countryName = $('#country-name').val();
 
-function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
-    $.getJSON(prefix + quoteUrl, createTweet);
-    $.ajaxSetup({ cache: false });
+  if(!countryName.length) countryName = 'Poland';
+
+  $.ajax({
+    url: url + countryName,
+    method: 'GET',
+    success: showCountriesList
+  });
 }
 
-//tweet
-function createTweet(input) {
-  var data = input[0];
+function showCountriesList(resp) {
+  countriesList.empty();
 
-  var quoteText = $(data.content).text().trim();
-  var quoteAuthor = data.title;
-
-  if (!quoteAuthor.length) {
-    quoteAuthor = "Unknown author";
-  }
-
-  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-
-  if (tweetText.length > 140) {
-    getQuote();
-  } else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text("Author: " + quoteAuthor);
-    $('.tweet').attr('href', tweet);
-  }
+  resp.forEach(function(item){
+      $('<li>').text(item.name).appendTo(countriesList);
+  });
 }
